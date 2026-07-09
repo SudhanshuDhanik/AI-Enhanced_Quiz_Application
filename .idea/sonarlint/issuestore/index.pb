efeb767c1116d8b16b77a2ea80ca534adbdcf80d
Sudhanshu Dhanik <sonudhanik2812@gmail.com>
2026-07-09 11:21:04 +0530

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+m
+=quiz/quiz/src/main/java/com/project/quiz/QuizApplication.java,0\4\04a880e3df4b811603c32f3e3840390475efff58
+{
+Kquiz/quiz/src/main/java/com/project/quiz/controller/QuestionController.java,c\0\c0906b6d54164f69c305e77472737dccce7cda7d
+u
+Equiz/quiz/src/main/java/com/project/quiz/service/QuestionService.java,8\7\876f86513c6386b90ae3e3a11d1c55387267c543
+{
+Kquiz/quiz/src/main/java/com/project/quiz/repository/QuestionRepository.java,9\9\9989974ed5b4933ae92a179eedcfb86aa72d0573
+l
+<quiz/quiz/src/main/java/com/project/quiz/model/Question.java,9\d\9dd878f90080332772adce9c71447f2a576c36a0
---
+++ QuizBackend/src/main/java/com/project/quiz/model/Question.java
@@ -0,0 +1,42 @@
+package com.project.quiz.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String question;
+
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String questionType;
+
+    @ElementCollection
+    @CollectionTable(name = "question_choices", joinColumns = @JoinColumn(name = "question_id"))
+    private List<String> choices;
+
+    @ElementCollection
+    @CollectionTable(name = "question_correct_answers", joinColumns = @JoinColumn(name = "question_id"))
+    private List<String> correctAnswers;
+
+    // ✅ Default constructor (required by JPA)
+    public Question() { }
+
+    public Question(long id, String javaBasics, int i) {
+        this.id = id;
+        this.question = javaBasics;
+    }
+}
